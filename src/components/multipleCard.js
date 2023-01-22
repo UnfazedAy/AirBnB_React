644b03426fe379e76324a82c2d538854bc36d6f6
UnfazedAy <ayomidesoniyi@gmail.com>
2023-01-22 09:19:09 +0100

    Modified it to use the map function to return various card elements

--- src/components/multipleCard.js
+++ src/components/multipleCard.js
@@ -1,35 +1,23 @@
-import athlete from '../assets/runner.png';
-import mountainBike from '../assets/biker.png';
-import bride from '../assets/wedding.png';
 import Card from '../components/Card';
+import data from '../components/data';
 
 export default function MultipleCard () {
+  const datasets = data.map((dataset) => {
+    return (
+      <Card
+        img={dataset.coverImg}
+        rating={dataset.stats.rating}
+        reviewCount={dataset.stats.reviewCount}
+        location={dataset.location}
+        title={dataset.title}
+        price={dataset.price}
+      />
+    )
+  });
+
   return (
-    <div className="identical">
-      <Card 
-        img={athlete}
-        rating={5.0}
-        reviewCount={6}
-        location='USA'
-        title='Life lessons with Katie Zafers'
-        price='136'
-      />
-      <Card 
-        img={bride}
-        rating={5.0}
-        reviewCount={30}
-        location='USA'
-        title='Life wedding photography'
-        price='125'
-      />
-      <Card 
-        img={mountainBike}
-        rating={4.80}
-        reviewCount={2}
-        location='USA'
-        title='Group mountain biking'
-        price='50'
-      />
+    <div className='identical'>
+      {datasets}
     </div>
   );
 }
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+
+// export default function App () {
+//   return (
+//     <div className="App">
+//       <Navbar />
+//       <Hero />
+//       <MultipleCard />
+//     </div>
+//   );
+// }
